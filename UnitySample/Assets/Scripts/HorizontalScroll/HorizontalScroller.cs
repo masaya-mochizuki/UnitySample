b239214e9d94cd37fa6c92262c14954bae52bcda
望月 正弥 <mochidzukimasaya@mochidzuki-no-MacBook-Air.local>
2016-09-22 15:35:38 +0900

    リストを要素ごとに横スクロール

---
+++ UnitySample/Assets/Scripts/HorizontalScroll/HorizontalScroller.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HorizontalScroller : MonoBehaviour {
+
+	public ScrollRect rect;
+	public int index = 0;
+	public float dulation = 1f;
+	public int Length { get; protected set; }
+	public bool IsScrolling { get; protected set; } //スクロール動作の実行中か
+
+	void Awake()
+	{
+		//要素数はcontentのchildCountから取る
+		Length = rect.content.childCount;
+	}
+
+	float GetTargetIndex () 
+	{
+		return (float)this.index / (float)(this.Length - 1);
+	}
+
+	public IEnumerator Scroll()
+	{
+		IsScrolling = true;
+
+		float target = GetTargetIndex ();
+		float now = rect.horizontalNormalizedPosition;
+
+		if (Mathf.Abs(target - now) < float.Epsilon) {
+			yield break;
+		}
+
+		float t = 0f;
+
+		var col = new WaitWhile(()=> {
+			t += Time.deltaTime;
+
+			rect.horizontalNormalizedPosition = Mathf.Lerp (now, target, t / dulation);
+
+			return t / dulation < 1f;
+		});
+
+		yield return col;
+
+		rect.horizontalNormalizedPosition = target;
+		IsScrolling = false;
+	}
+}
---
+++ UnitySample/Assets/Scripts/HorizontalScroll/ScrollSample.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollSample : MonoBehaviour {
+
+	[SerializeField]HorizontalScroller scroller;
+
+	public void Next()
+	{
+		UpdateIndex(Mathf.Min(scroller.index+1,scroller.Length-1));
+	}
+
+	public void Prev()
+	{
+		UpdateIndex(Mathf.Max(scroller.index-1,0));
+	}
+
+	public void First()
+	{
+		UpdateIndex(0);
+	}
+
+	public void Last()
+	{
+		UpdateIndex(scroller.Length-1);
+	}
+
+	public void UpdateIndex(int index)
+	{
+		if ( scroller.IsScrolling || scroller.index == index) {
+			return;
+		}
+
+		scroller.index = index;
+		StartCoroutine(scroller.Scroll());
+	}
+
+}
